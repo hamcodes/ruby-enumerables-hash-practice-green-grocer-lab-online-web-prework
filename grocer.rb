00334afb58bc63557f3d4b9a9a8d290797c47e5a
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-07 17:12:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,48 @@
+
 def consolidate_cart(cart)
-  # code here
+  updated_cart = {} 
+  cart.each do |item|
+    if updated_cart[item.keys[0]] 
+      updated_cart[item.keys[0]][:count] += 1
+    else
+
+      updated_cart[item.keys[0]] = {
+            count: 1,
+            price: item.values[0][:price],
+            clearance: item.values[0][:clearance]
+      }
+    end
+  end
+updated_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    if cart.keys.include?(coupon[:item])
+      if cart[coupon[:item]][:count] >= coupon[:num]
+        discount = "#{coupon[:item]} W/COUPON"
+        if cart[discount] 
+          cart[discount][:count] += coupon[:num]
+        else
+          cart[discount] = {}
+          cart[discount] = {:price => coupon[:cost]/coupon[:num], :clearance => cart[coupon[:item]][:clearance], :count => coupon[:num]}
+          end
+        cart[coupon[:item]][:count] -= coupon[:num]
+      end    
+    end
+  end 
+  cart
 end
+
+
 
 def apply_clearance(cart)
-  # code here
+
 end
 
+
 def checkout(cart, coupons)
-  # code here
+
+
 end
+
